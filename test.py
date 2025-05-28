7a21788837c982db282f1ab6a2b5b4ac0099714f
Yash Ahlawat <81874537+Yahlawat@users.noreply.github.com>
2025-05-27 23:12:12 -0400

    Remove test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-from transformers.pipelines import pipeline
-
-
-try:
-    pipe = pipeline("text-generation", model="tiiuae/falcon-rw-1b", local_files_only=True)
-    print("✅ Model loaded locally.")
-except Exception as e:
-    print("❌ Model not found locally:", e)
